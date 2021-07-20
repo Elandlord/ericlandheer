9eddff8b15ac0976facada6a98a9100dc345e569
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-20 14:08:43 +0200

    :rainbow: Add banner animations + list of skills

--- resources/views/pages/homepage/partials/banner.blade.php
+++ resources/views/pages/homepage/partials/banner.blade.php
@@ -1,7 +1,7 @@
-<div class="container mx-auto mb-100">
+<div class="container mx-auto mb-20">
     <div class="flex flex-col ">
         <div class="absolute top-0 left-0 w-full h-100 xl:h-190">
-            <ul class="skills">
+            <ul class="skills hidden md:block">
                 <li>
                     <img src="assets/images/laravel.png"/>
                 </li>
@@ -34,17 +34,39 @@
                 </li>
             </ul>
         </div>
-        <div class="grid grid-cols-2 gap-4 py-20 relative">
-            <div class="w-1/2 rounded-full overflow-hidden items-center">
-                <img class="w-full h-full object-cover" src="assets/images/eric-resume.png" />
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 py-20 relative mx-8 lg:mx-0">
+            <div class="flex justify-center">
+                <div class="w-1/2 lg:w-full xl:w-1/2 rounded-full overflow-hidden">
+                    <img class="w-full h-full object-cover" src="assets/images/eric-resume.png" />
+                </div>
             </div>
-            <div class="flex flex-col justify-center">
-                <h1 class="text-blue-400 text-3xl sm:text-5xl sm:leading-none font-extrabold tracking-tight mb-4">
+
+            <div class="flex flex-col justify-center text-center lg:text-left">
+                <h1 class="text-blue-400 text-3xl sm:text-5xl sm:leading-none font-extrabold leading-loose mb-4">
                     Eric Landheer
                 </h1>
-                <h2 class="text-gray-800 text-2xl sm:text-3xl sm:leading-none font-extrabold tracking-wide mb-4">
+                <h2 class="text-gray-800 text-2xl sm:text-3xl sm:leading-none font-bold leading-loose mb-4">
                     Full stack webdeveloper
                 </h2>
+
+                <p class="text-black tracking-wide leading-loose">
+                    Since writing my first line of HTML and PHP at the age of 15, I have had a huge interest in the web and the development of it.
+                    I graduated HBO-ICT at the Hanzehogeschool Groningen in early 2020.
+                </p>
+
+                <p class="text-black tracking-wide leading-loose mt-4">
+                    Anything that runs on the web sparks my interest, including: microservices, databases and API's.. As long as it can be connected.
+                </p>
+
+                <p class="text-black tracking-wide leading-loose mt-4">
+                    The aforementioned connecting (and building), I mostly do using <a href="">Laravel</a> and <a href="">Vue.js</a>, and have been playing around
+                    with <a href="">Go</a> and <a href="">Firebase</a> as of late.
+                </p>
+
+{{--                <p class="text-black tracking-wide leading-loose mt-4">--}}
+{{--                    Although PHP is viewed as an old and outdated language, it yet still powers <a href="https://kinsta.com/blog/is-php-dead/"> almost 8 out of 10 of the websites</a>.--}}
+{{--                    The improvements of the language as of PHP 7.4 and PHP 8.0, makes PHP an adult language that is here to stay.--}}
+{{--                </p>--}}
             </div>
         </div>
     </div>
--- resources/views/pages/homepage/partials/skills.blade.php
+++ resources/views/pages/homepage/partials/skills.blade.php
@@ -1,3 +1,127 @@
-<div class="container mx-auto">
-    Test
+<div class="py-12 bg-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="lg:text-center">
+            <h2 class="text-base text-blue-400 font-semibold tracking-wide uppercase">In level of mastery</h2>
+            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Skills
+            </p>
+            <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                Below are the various skills and techniques I have acquired over the years.
+            </p>
+        </div>
+
+        <div class="mt-10">
+            <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                <div class="relative">
+                    <dt>
+                        <div class="absolute flex items-center justify-center h-12 w-12 ">
+                            <img src="/assets/images/laravel.png" />
+                        </div>
+                        <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Laravel</p>
+                    </dt>
+                    <dd class="mt-2 ml-16 text-base text-gray-500">
+                        Laravel has been my go-to framework in PHP since 2015. I have been working with it professionally for years,
+                        and I have yet to encounter a (back-end) problem I could not solve with it.
+                    </dd>
+                </div>
+
+                <div class="relative">
+                    <dt>
+                        <div class="absolute flex items-center justify-center h-12 w-12 rounded-md">
+                            <img src="/assets/images/vue.png" />
+
+                        </div>
+                        <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Vue.js</p>
+                    </dt>
+                    <dd class="mt-2 ml-16 text-base text-gray-500">
+                        Vue.js feels like a huge upgrade coming from Vanilla Javascript or jQuery. Even though other major
+                        JS frameworks (React, Angular) offer a similar amount of functionality, I still prefer Vue.js as
+                        it integrates seamlessly in Laravel projects.
+                    </dd>
+                </div>
+
+                <div class="relative">
+                    <dt>
+                        <div class="absolute flex items-center justify-center h-12 w-12 rounded-md">
+                            <img src="/assets/images/tailwind.png" />
+                        </div>
+                        <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Tailwind CSS</p>
+                    </dt>
+                    <dd class="mt-2 ml-16 text-base text-gray-500">
+                        Semantic classes like: top-navigation-header simply do not feel good. My colleague and I decided that in 2015.
+                        We decided to use our own small, utility-first CSS framework (with its caveats).
+                        Since Tailwindcss came along, we have used that in our projects and never looked back.
+                    </dd>
+                </div>
+
+                <div class="relative">
+                    <dt>
+                        <div class="absolute flex items-center justify-center h-12 w-12 rounded-md">
+                            <img src="/assets/images/docker.png" />
+                        </div>
+                        <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Docker</p>
+                    </dt>
+                    <dd class="mt-2 ml-16 text-base text-gray-500">
+                        <i>But it works on my local machine?</i>
+                        This sentence should be a thing of the past using Docker. I have been using Docker locally for years.
+                        Even though I do not fully master deployment and orchestration with Docker (yet!), I do see the benefits it has and am <strong>keen</strong>
+                        to develop my Docker-skills this the coming months.
+                    </dd>
+                </div>
+
+                <div class="relative">
+                    <dt>
+                        <div class="absolute flex items-center justify-center h-12 w-12 rounded-md">
+                            <img src="/assets/images/go.png" />
+                        </div>
+                        <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Go</p>
+                    </dt>
+                    <dd class="mt-2 ml-16 text-base text-gray-500">
+                        What else can I say about Go besides: <i>"Go use it"</i>! It is small, relatively simple to learn and
+                        scales really well. Even though Laravel (using PHP) is my go-to Framework for web-solutions, I have
+                        been tinkering around with Go and see the possibilities of the language.
+                    </dd>
+                </div>
+
+                <div class="relative">
+                    <dt>
+                        <div class="absolute flex items-center justify-center h-12 w-12 rounded-md">
+                            <img src="/assets/images/flutter.png" />
+                        </div>
+                        <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Flutter</p>
+                    </dt>
+                    <dd class="mt-2 ml-16 text-base text-gray-500">
+                        Despite the fact that I have never launched an app through the Google Play Store / Apple App Store, I have occasionally worked with Flutter. I trust to one day launch a full-fledged app with Flutter.
+                    </dd>
+                </div>
+
+                <div class="relative">
+                    <dt>
+                        <div class="absolute flex items-center justify-center h-12 w-12 rounded-md">
+                            <img src="/assets/images/firebase.png" />
+                        </div>
+                        <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Firebase</p>
+                    </dt>
+                    <dd class="mt-2 ml-16 text-base text-gray-500">
+                        Firebase is on my list to learn. As a small hobby side-project, I have recently developed a small, simple application that allows users
+                        to keep track of the location of their items. Using Firestore has been a different taste than old trusty Laravel and
+                        a MySQL database.
+                    </dd>
+                </div>
+
+                <div class="relative">
+                    <dt>
+                        <div class="absolute flex items-center justify-center h-12 w-12 rounded-md">
+                            <img src="/assets/images/question.png" />
+                        </div>
+                        <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Something new?</p>
+                    </dt>
+                    <dd class="mt-2 ml-16 text-base text-gray-500">
+                        If you think I'm missing out on an essential skill or technique, please let me know at: <a class="text-blue-400" href="">Test</a>
+                    </dd>
+                </div>
+
+            </dl>
+        </div>
+    </div>
 </div>
