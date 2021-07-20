354fb304b0500398a487b1bc8dccd843bea1037f
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-20 14:15:45 +0200

    :rainbow: Add footer

---
+++ resources/views/master/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-blue-400">
+    <div class="flex text-white justify-center py-4">
+        <p class="text-center">
+            &copy Eric Landheer {{ Date('Y') }}
+        </p>
+    </div>
+</footer>
