80b850f39d8cec46661f3fff8da4f9c2d5e12f90
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-20 12:43:29 +0200

    :rainbow: Add moving banner with animations

---
+++ resources/views/pages/homepage/index.blade.php
@@ -0,0 +1,6 @@
+@extends('master.master')
+
+@section('content')
+    @include('pages.homepage.partials.banner')
+    @include('pages.homepage.partials.skills')
+@endsection
---
+++ resources/views/pages/homepage/partials/banner.blade.php
@@ -0,0 +1,51 @@
+<div class="container mx-auto mb-100">
+    <div class="flex flex-col ">
+        <div class="absolute top-0 left-0 w-full h-100 xl:h-190">
+            <ul class="skills">
+                <li>
+                    <img src="assets/images/laravel.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/finder.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/flutter.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/php.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/vue.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/docker.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/go.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/kubernetes.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/node.png"/>
+                </li>
+                <li>
+                    <img src="assets/images/tailwind.png"/>
+                </li>
+            </ul>
+        </div>
+        <div class="grid grid-cols-2 gap-4 py-20 relative">
+            <div class="w-1/2 rounded-full overflow-hidden items-center">
+                <img class="w-full h-full object-cover" src="assets/images/eric-resume.png" />
+            </div>
+            <div class="flex flex-col justify-center">
+                <h1 class="text-blue-400 text-3xl sm:text-5xl sm:leading-none font-extrabold tracking-tight mb-4">
+                    Eric Landheer
+                </h1>
+                <h2 class="text-gray-800 text-2xl sm:text-3xl sm:leading-none font-extrabold tracking-wide mb-4">
+                    Full stack webdeveloper
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
