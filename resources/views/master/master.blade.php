f93fb2bd9412125bdaa0faf2b78adc91484665ae
Eric Landheer <5966835+Elandlord@users.noreply.github.com>
2021-07-20 12:43:29 +0200

    :rainbow: Add moving banner with animations

---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>Eric Landheer | Webdeveloper | Interactive resume</title>
+
+</head>
+</html>
+<body class="bg-gray-50">
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,58 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px'
+        },
+
+        colors: {
+
+        },
+
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+
+        spacing: {
+            px: '1px',
+            '0': '0',
+            '1': '0.25rem',
+            '2': '0.5rem',
+            '3': '0.75rem',
+            '4': '1rem',
+            '5': '1.25rem',
+            '6': '1.5rem',
+            '8': '2rem',
+            '10': '2.5rem',
+            '12': '3rem',
+            '14': '3.5rem',
+            '16': '4rem',
+            '18': '4.5rem',
+            '20': '5rem',
+            '22': '5.5rem',
+            '24': '6rem',
+            '28': '7rem',
+            '32': '8rem',
+            '40': '10rem',
+            '48': '12rem',
+            '56': '14rem',
+            '64': '16rem',
+            '84': '21rem',
+            '100': '25rem',
+            '110': '27.5rem',
+            '130': '32.5rem',
+            '160': '40rem',
+            '190': '47.5rem',
+            '210': '52.5rem',
+            '230': '57.5rem',
+        },
+    },
   },
   variants: {
     extend: {},
