3f8d171cd45dab358a0b639be399b6e33b85b3d0
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-23 11:01:12 +0200

    :rainbow: Correct authorization Laravel Nova

--- app/Http/Controllers/Pages/HomepageController.php
+++ app/Http/Controllers/Pages/HomepageController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
 use App\Models\Experience;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class HomepageController extends Controller
 {
