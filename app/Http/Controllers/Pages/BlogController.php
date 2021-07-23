8c65c54e8c7662e4d92f9b73d671e311c207a02e
Eric Landheer <5966835+Elandlord@users.noreply.github.com>
2021-07-23 12:35:27 +0200

    :rainbow: Add nav + blog

--- app/Http/Controllers/Pages/BlogController.php
+++ app/Http/Controllers/Pages/BlogController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
+use App\Models\BlogPost;
 use Illuminate\Http\Request;
+use Spatie\ShikiPhp\Shiki;
 
 class BlogController extends Controller
 {
     public function index()
     {
-        return view('pages.blog.index');
+        $blogPosts = BlogPost::all();
+
+        return view('pages.blog.index', compact('blogPosts'));
+    }
+
+    public function show(BlogPost $blogPost)
+    {
+        return view('pages.blog-posts.index', compact('blogPost'));
     }
 }
---
+++ resources/views/pages/blog-posts/index.blade.php
@@ -0,0 +1,6 @@
+@extends('master.master')
+
+@section('content')
+    @include('pages.blog-posts.partials.banner')
+    @include('pages.blog-posts.partials.content')
+@endsection
