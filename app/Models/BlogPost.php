3adce21ce8854c9e951823a02697a0af3c7e8660
Eric Landheer <5966835+Elandlord@users.noreply.github.com>
2021-07-23 13:13:41 +0200

    :rainbow: Reduce load to store blog post format while saving

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use App\Casts\FlexibleBody;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Casts\AsCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -25,16 +28,38 @@ class BlogPost extends Model implements HasMedia
         'slug',
         'intro',
         'body',
+        'formatted_body',
     ];
 
     protected $casts = [
         'body' => FlexibleCast::class,
+        'formatted_body' => AsArrayObject::class,
     ];
 
     protected static function booted()
     {
-        static::saving(function ($blogPost) {
+        static::saving( function ($blogPost) {
             $blogPost->slug = Str::slug($blogPost->title);
+        });
+
+        static::saved( function ($blogPost) {
+            $blogPost->formatted_body = $blogPost->body->map( function($contentPart) {
+                $content = $contentPart['attributes']['section'];
+
+                $code = Str::between($content, '-beginCode', '-endCode');
+
+                $formattedBody = Str::replaceFirst('-beginCode', '', $content);
+                $formattedBody = Str::replaceFirst('-endCode', '', $formattedBody);
+
+                $formattedBody = Str::replace($code, Shiki::highlight(
+                    code: $code,
+                    language: 'php',
+                    theme: 'github-light',
+                ), $formattedBody);
+
+                return $formattedBody;
+            });
+            $blogPost->saveQuietly();
         });
     }
 
@@ -48,28 +73,6 @@ class BlogPost extends Model implements HasMedia
         return optional($this->getMedia('blog_image')
             ->first())
             ->getUrl() ?? 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80';
-    }
-
-    public function getFormattedBodyAttribute(): Collection
-    {
-        $contentParts = $this->body->map( function($contentPart) {
-            $content = $contentPart->getAttributes()['section'];
-
-            $code = Str::between($content, '-beginCode', '-endCode');
-
-            $formattedBody = Str::replaceFirst('-beginCode', '', $content);
-            $formattedBody = Str::replaceFirst('-endCode', '', $formattedBody);
-
-            $formattedBody = Str::replace($code, Shiki::highlight(
-                code: $code,
-                language: 'php',
-                theme: 'github-light',
-            ), $formattedBody);
-
-            return $formattedBody;
-        });
-
-        return $contentParts;
     }
 
     public function registerMediaConversions(Media $media = null): void
