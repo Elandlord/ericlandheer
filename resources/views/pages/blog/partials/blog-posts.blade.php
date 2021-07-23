3926a4ada14bf08db8157edfe5bce632ad109dcc
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-23 16:47:45 +0200

    :rainbow: Push tags + created date to bottom of card

--- resources/views/pages/blog/partials/blog-posts.blade.php
+++ resources/views/pages/blog/partials/blog-posts.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto py-20">
         <div class="grid lg:grid-cols-2 xl:grid-cols-3 gap-8 text-center">
             @forelse($blogPosts as $blogPost)
-                <div class="py-8 px-8 bg-white shadow-lg rounded-lg my-8">
+                <div class="flex flex-col flex-1 py-8 px-8 bg-white shadow-lg rounded-lg my-8">
                     <div class="flex justify-center md:justify-end -mt-16">
                         <img class="w-20 h-20 object-cover rounded-full border-2 border-blue-400" src="{{ $blogPost->blogImage }}">
                     </div>
@@ -10,16 +10,19 @@
                         <h2 class="text-gray-800 text-2xl font-semibold">{{ $blogPost->title }}</h2>
                         <p class="mt-2 text-gray-600 py-4">{!! strip_divs($blogPost->intro) !!}</p>
                     </div>
-                    <div class="flex mt-4">
-                        @forelse($blogPost->tags as $tag)
-                            <span class="text-white bg-blue-400 py-1 px-2 rounded-md mr-2">{{ $tag->name }}</span>
-                        @empty
-                        @endforelse
+                    <div class="mt-auto">
+                        <div class="flex mt-4">
+                            @forelse($blogPost->tags as $tag)
+                                <span class="text-white bg-blue-400 py-1 px-2 rounded-md mr-2">{{ $tag->name }}</span>
+                            @empty
+                            @endforelse
+                        </div>
+                        <div class="flex justify-between mt-4">
+                            <a href="{{ route('pages.blog.detail', ['blogPost' => $blogPost->slug]) }}" class="text-base font-medium text-blue-400">Read more</a>
+                            <p class="text-base font-medium text-blue-400">{{ $blogPost->created_at->diffForHumans() }}</p>
+                        </div>
                     </div>
-                    <div class="flex justify-between mt-4">
-                        <a href="{{ route('pages.blog.detail', ['blogPost' => $blogPost->slug]) }}" class="text-base font-medium text-blue-400">Read more</a>
-                        <p class="text-base font-medium text-blue-400">{{ $blogPost->created_at->diffForHumans() }}</p>
-                    </div>
+
                 </div>
             @empty
                 <h3 class="text-xl font-bold">🤨 Nothing found</h3>
