9c9afb0bf267f595b449e9e14bcdbe8105a24b48
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-23 13:21:00 +0200

    :rainbow: Add image to blog post page

--- resources/views/pages/blog-posts/partials/banner.blade.php
+++ resources/views/pages/blog-posts/partials/banner.blade.php
@@ -1,13 +1,21 @@
 <div class="container mx-auto py-20">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="lg:text-center">
-            <h2 class="text-base text-blue-400 font-semibold tracking-wide uppercase">{{ $blogPost->created_at->diffForHumans() }}</h2>
-            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                {{ $blogPost->title }}
-            </p>
-            <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                {{ $blogPost->intro }}
-            </p>
+        <div class="flex flex-col md:flex-row items-center">
+            <div class="flex justify-center mb-4 md:mr-12">
+                <div class="w-40 h-40 md:w-100 md:h-100 rounded-full overflow-hidden">
+                    <img class="w-full h-full" src="{{ $blogPost->blogImage }}" />
+                </div>
+            </div>
+            <div>
+                <h2 class="text-base text-blue-400 font-semibold tracking-wide uppercase">{{ $blogPost->created_at->diffForHumans() }}</h2>
+                <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                    {{ $blogPost->title }}
+                </p>
+                <p class="mt-4 max-w-2xl text-xl text-gray-500 leading-loose lg:mx-auto">
+                    {{ $blogPost->intro }}
+                </p>
+            </div>
+
         </div>
     </div>
 </div>
