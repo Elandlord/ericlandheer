46c61c33c34c9fbccaf2b7490f3bb9249307b037
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-26 11:33:10 +0200

    :rainbow: Remove horizontal scroll on lg screens

--- resources/views/pages/blog-posts/partials/content.blade.php
+++ resources/views/pages/blog-posts/partials/content.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto py-20">
         <div class="px-4 sm:px-6 lg:px-0">
             <div>
-                <div class="overflow-x-scroll lg:overflow-x-auto">
+                <div class="overflow-x-scroll lg:overflow-x-hidden">
                     @forelse($blogPost->formatted_body as $contentPart)
                         <div>
                             {!! $contentPart !!}
