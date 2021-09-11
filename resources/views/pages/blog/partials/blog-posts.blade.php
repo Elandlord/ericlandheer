acd2365e01f667c60446784c2944a2feb6fc3305
Eric Landheer <5966835+Elandlord@users.noreply.github.com>
2021-09-11 17:46:49 +0200

    :rainbow: Retrigger loading transition + increase debounce time

--- resources/views/pages/blog/partials/blog-posts.blade.php
+++ resources/views/pages/blog/partials/blog-posts.blade.php
@@ -1,14 +1,14 @@
 <div class="bg-white">
     @if( empty($tag))
         <div class="container mx-auto">
-            <input wire:model="search" type="text" class="w-full h-full border-2 border-gray-300 py-2 px-4 rounded-md my-8" placeholder="Search here..">
+            <input wire:model.debounce.500ms="search" type="text" class="w-full h-full border-2 border-gray-300 py-2 px-4 rounded-md my-8" placeholder="Search here..">
         </div>
     @endif
     <div class="container mx-auto py-20">
-        <div wire:loading.delay wire:target="search">
+        <div wire:loading.delay wire:loading.remove.delay wire:target="search">
             <p class="text-center">Loading blogposts...</p>
         </div>
-        <div wire:loading.remove.delay wire:target="search" class="grid lg:grid-cols-2 xl:grid-cols-3 gap-8 text-center animate-fade-in-down" >
+        <div wire:loading.remove.delay wire:target="search" wire:loading.class.remove="animate-fade-in-down" class="grid lg:grid-cols-2 xl:grid-cols-3 gap-8 text-center animate-fade-in-down" >
             @forelse($blogPosts as $blogPost)
                 @include('pages.blog.partials.blog-card')
             @empty
