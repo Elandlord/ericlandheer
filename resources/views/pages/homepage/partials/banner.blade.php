f3ff10c63aaaad85204c3fd65ed25e9aaa13276f
Eric Landheer <5966835+Elandlord@users.noreply.github.com>
2021-10-07 17:42:49 +0200

    :rainbow: Removed open to job offer content

--- resources/views/pages/homepage/partials/banner.blade.php
+++ resources/views/pages/homepage/partials/banner.blade.php
@@ -98,14 +98,14 @@
                             <a href="mailto:ericlandheer95@gmail.com" class="font-bold">ericlandheer95@gmail.com</a>
                         </div>
                     </div>
-                    <div class="flex items-center mt-4 md:mt-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-400 h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z" clip-rule="evenodd" />
-                        </svg>
+{{--                    <div class="flex items-center mt-4 md:mt-0">--}}
+{{--                        <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-400 h-8 w-8" viewBox="0 0 20 20" fill="currentColor">--}}
+{{--                            <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z" clip-rule="evenodd" />--}}
+{{--                        </svg>--}}
 {{--                        <div class="ml-4">--}}
 {{--                            <p class="font-bold">Open to job offers (+- 24h)</p>--}}
 {{--                        </div>--}}
-                    </div>
+{{--                    </div>--}}
                     <div class="flex items-center mt-4">
                         <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-400 h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
