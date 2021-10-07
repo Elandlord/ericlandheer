4a6e65ab4d18c38753df5cf4d4b8d427372c8507
Eric Landheer <e.landheer@mentechmedia.nl>
2021-10-07 17:42:41 +0200

    gs

--- resources/views/pages/homepage/partials/banner.blade.php
+++ resources/views/pages/homepage/partials/banner.blade.php
@@ -102,9 +102,9 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-400 h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z" clip-rule="evenodd" />
                         </svg>
-                        <div class="ml-4">
-                            <p class="font-bold">Open to job offers (+- 24h)</p>
-                        </div>
+{{--                        <div class="ml-4">--}}
+{{--                            <p class="font-bold">Open to job offers (+- 24h)</p>--}}
+{{--                        </div>--}}
                     </div>
                     <div class="flex items-center mt-4">
                         <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-400 h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
